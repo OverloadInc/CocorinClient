c3a4330e17af662b8791ceb9554bcff4f4c2938b
Overload-Inc <64666439+overload-inc@users.noreply.github.com>
2020-06-06 19:29:40 -0500

    ButtonListener class added to control actions performed by the chat's send button

---
+++ src/over/controller/ButtonListener.java
@@ -0,0 +1,37 @@
+package over.controller;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class ButtonListener implements MouseListener {
+    private JButton btnSend;
+
+    public ButtonListener(JButton btnSend) {
+        this.btnSend = btnSend;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        btnSend.setIcon(new ImageIcon(getClass().getResource("/over/res/img/open_mail_01.png")));
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        btnSend.setIcon(new ImageIcon(getClass().getResource("/over/res/img/close_mail_01.png")));
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        btnSend.setIcon(new ImageIcon(getClass().getResource("/over/res/img/open_mail_01.png")));
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        btnSend.setIcon(new ImageIcon(getClass().getResource("/over/res/img/close_mail_01.png")));
+    }
+}
